7a2897e4812060ec1d50fe10909599755f54d143
Nataly Shrits <nataly87s@gmail.com>
2018-03-09 00:30:00 +0200

    added firebase login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+
 import logo from './logo.svg';
 import './App.css';
+
+async function login() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  try {
+    const result = await firebase.auth().signInWithPopup(provider);
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+
+    alert(`logged in ${user}`);
+  } catch (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    alert(`error ${error}`);
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={login}>login</button>
       </div>
     );
   }
---
+++ src/init-firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyC-w0m0X4CPu5BgaLK4hCqiOx85w_J7K_8",
+  authDomain: "gofetch-11c3f.firebaseapp.com",
+  databaseURL: "https://gofetch-11c3f.firebaseio.com",
+  projectId: "gofetch-11c3f",
+  storageBucket: "gofetch-11c3f.appspot.com",
+  messagingSenderId: "846821050292"
+};
+firebase.initializeApp(config);
