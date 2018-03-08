6ecb65a3fb5aa1bc5c4d9756b45d03c96badc14f
Nataly Shrits <nataly87s@gmail.com>
2018-03-09 00:09:03 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NoMatch = () => 'no match :(';
+
+const Main = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route component={NoMatch}/>
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
 registerServiceWorker();
