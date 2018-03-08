8819f3098c1138799a4548b58eaabc20abe19b4f
Yasmin <Yas@email.com>
2018-03-09 01:40:29 +0200

    ..

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import login from '../App';
+import logo from '../logo.svg';
 import './Login.css';
 import '../App.css';
 
@@ -6,19 +8,26 @@ class Login extends Component {
     render() {
       return (
         <div>
-          <header className="App-header">
-            <h1 className="App-title">Looking</h1>
-          
-            <p className="Page-logins">
-                <button className="Logins-buttons">To Adopt</button>
-                <button className="Logins-buttons">For a Home</button>
+            <header className="Login-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="Login-title">GoFetch</h1>
+            </header>
+            <p className="App-intro">
+                Get to know your next pet friend
             </p>
-          </header>
-          <p>
-            <button>Facebook Login</button>
-            <br/>
-            <button>Google Login</button>
-          </p>
+            <header className="Logins-header">
+                <h1 className="App-title">Looking...</h1>
+            
+                <form className="Page-logins">
+                    <input type="radio" className="Logins-buttons" name="Looking" value="Adopt" />To Adopt
+                    <input type="radio" className="Logins-buttons" name="Looking" value="Owner" />For a Home
+                </form>
+            </header>
+            <p className="User-logins">
+                <button onClick={this.props.loginFacebook}>Facebook Login</button>
+                <br/>
+                <button onClick={this.props.loginFacebook}>Google Login</button>
+            </p>
         </div>
       );
     }
