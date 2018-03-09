d6879f187c4320ef3cc4b73b20ddb6c3a1e2eff6
Yasmin <Yas@email.com>
2018-03-09 03:48:43 +0200

    ///

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import login from '../App';
-import logo from '../logo.svg';
+import logo from '../resources/logo.svg';
 import './Login.css';
 import '../App.css';
 
 class Login extends Component {
+     
     render() {
-      return (
+        const onRadioChange = (event) => {
+            this.setState({value: event.target.value});
+        }
+        
+      return (      
         <div>
-            <header className="Login-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="Login-title">GoFetch</h1>
-            </header>
-            <p className="App-intro">
-                Get to know your next pet friend
-            </p>
-            <header className="Logins-header">
-                <h1 className="App-title">Looking...</h1>
+            <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet" />
+            <div className="Logins-header">
+                <img src={logo} className="logo" alt="logo"/>
+                <p className="Logins-intro">
+                    Get to know your next pet friend
+                </p>
+                <p className="LOOKING">Looking...</p>
+            </div>
             
+            <div className="Logins-footer">
                 <form className="Page-logins">
-                    <input type="radio" className="Logins-buttons" name="Looking" value="Adopt" />To Adopt
-                    <input type="radio" className="Logins-buttons" name="Looking" value="Owner" />For a Home
+                    <label>
+                        <input type="radio" className="ChoosePage" name="Looking" value="Adopt" onChange={onRadioChange} />
+                        <img src="'img/resources/to-adopt-icon-active.svg" />
+                        <div className="TO-ADOPT">To Adopt</div>
+                    </label>
+                    <label>
+                        <input type="radio" className="ChoosePage" name="Looking" value="Owner" onChange={onRadioChange} />
+                        <img src="'../resources/for-a-home-icon-inactive.svg" />
+                        <div className="FOR-A-HOME">For a Home</div>
+                    </label>
                 </form>
-            </header>
+            </div>
             <p className="User-logins">
-                <button onClick={this.props.loginFacebook}>Facebook Login</button>
+                <button onClick={() => this.props.loginFacebook(this.props, this.state.value)}>Facebook Login</button>
                 <br/>
-                <button onClick={this.props.loginFacebook}>Google Login</button>
+                <button onClick={() => this.props.loginGoogle(this.props, this.state.value)}>Google Login</button>
             </p>
         </div>
       );
