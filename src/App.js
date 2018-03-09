2de861217081d41c3c429655b0d48bd709f928af
Yasmin <Yas@email.com>
2018-03-09 07:09:05 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import Login from './pages/Login';
-
-<<<<<<< HEAD
-=======
-import logo from './resources/logo.svg';
->>>>>>> 4bb1595e71ff8b9cab6d1312c529552e72901609
 import './App.css';
 
 class App extends Component {
