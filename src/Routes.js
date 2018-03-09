4bb1595e71ff8b9cab6d1312c529552e72901609
Nataly Shrits <nataly87s@gmail.com>
2018-03-09 05:40:24 +0200

    redirect to login

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
-import App from './App';
 import AdoptProfile from './pages/profile/AdoptProfile';
+import Login from './pages/Login';
+
 const NoMatch = () => 'no match :(';
 
 export default () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={App}/>
+      <Route exact path="/" render={() => <Redirect to="/login" />}/>
+      <Route path="/login" component={Login}/>
       <Route path="/adopt" component={AdoptProfile} />
       <Route component={NoMatch}/>
     </Switch>
